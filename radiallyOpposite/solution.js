83986f63d994d1bac5e9450c84412d5659367fe6
eliudnyamai <eliudmitau@gmail.com>
2021-03-14 15:18:54 +0300

    radially opposite solved

---
+++ radiallyOpposite/solution.js
@@ -0,0 +1,13 @@
+// check problem document for explanation
+function circleOfNumbers(n, firstNumber) {
+  var half = n / 2;
+  var radiallyOpposite;
+  firstNumber >= half
+    ? (radiallyOpposite = firstNumber - half)
+    : (radiallyOpposite = firstNumber + half);
+  return radiallyOpposite;
+}
+// circleOfNumbers(10,5)==0
+// circleOfNumbers(10, 2) == 8
+// circleOfNumbers(10, 3) == 7;
+// circleOfNumbers(10, 8) == 2;
